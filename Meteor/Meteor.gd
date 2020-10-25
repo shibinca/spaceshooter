40bf0635b954a6c5d5043dcbba542ddcae5c5648
Shibin <shibin@pop-os.localdomain>
2020-10-24 20:40:15 -0400

    initial

---
+++ Meteor/Meteor.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+export var minSpeed: float = 10
+export var maxSpeed: float = 20
+export var minRoationRate: float = -20
+export var maxRoationRate: float = 20
+
+export var life: int = 20 
+
+var speed: float = 0
+var rotationRate: float = 0
+
+func _ready():
+	speed = rand_range(minSpeed, maxSpeed)
+	rotationRate = rand_range(minRoationRate, maxRoationRate)
+	
+
+func _physics_process(delta):
+	
+	rotation_degrees += rotationRate * delta	
+	position.y += speed * delta
+	
+
+func damage(amount: int):
+	life -= amount
+	if life <= 0:
+		queue_free()
+	
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
